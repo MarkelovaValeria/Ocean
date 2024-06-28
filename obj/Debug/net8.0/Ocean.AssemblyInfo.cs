adf178fed4c40a2206a71ac6a440d5d0c7a142d1
MarkelovaValeria <markelovav896@gmail.com>
2024-06-29 00:11:39 +0300

    update style

--- obj/Debug/net8.0/Ocean.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ocean.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ocean")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+193b78af5bb171e87a72c79c55a065d019637eb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e828e459febcb6495aefccda9c7bcf07776c40d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ocean")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ocean")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
